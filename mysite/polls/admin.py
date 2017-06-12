a2c396c388997f8238b6e07c1999f48e4c9b611e
ahnYH <ahnyh27@kotect.co.kr>
2017-06-12 13:27:16 +0900

    22

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,6 +4,12 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from polls.models import Question, Choice
 
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question Statement', {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
